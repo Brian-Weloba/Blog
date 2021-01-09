f11a3a35cdaf0ea8cd4084f3f4eea462fa65953e
Brian <bweloba@gmail.com>
2021-01-09 13:46:53 +0300

    Change to post method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import jdk.nashorn.internal.runtime.regexp.joni.ast.StringNode;
+import models.Post;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -14,6 +15,13 @@ public class App{
         get("/",(request, response) -> {
             Map<String, Object> model= new HashMap<String, Object>();
             return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/posts/new",(request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String content = request.queryParams("content");
+            Post newPost = new Post(content);
+            return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
