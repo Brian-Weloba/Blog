0a4f14b27e9a2920413b4021167ea2c8037c2082
Brian <bweloba@gmail.com>
2021-01-09 12:58:23 +0300

    create Post object and test it

--- src/test/java/models/PostTest.java
+++ src/test/java/models/PostTest.java
@@ -9,21 +9,21 @@ import static org.junit.Assert.*;
 public class PostTest {
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() {
     }
 
     @Test
-    public void Post_canInstantiate_true() throws Exception{
+    public void Post_canInstantiate_true() {
         Post post = new Post();
         assertTrue(post instanceof Post);
     }
 
     @Test
-    public void Post_canInstantiateWithContent_true() throws Exception{
+    public void Post_canInstantiateWithContent_true() {
         Post post = new Post("Day 1: Intro");
         assertEquals("Day 1: Intro", post.getContent());
     }
